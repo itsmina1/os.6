1f4f865abb85bdf8c7a3acc105151466812ae321
itsmina1 <mina.adham1999@gmail.com>
2021-05-12 15:41:19 -0700

    sh

---
+++ 6.sh
@@ -0,0 +1,21 @@
+ ls -l
+ ls -ls
+ ls -r
+ ls -ltr
+ 
+ ----------------------
+ 
+ sudo apt-get install rar
+ mkdir myfile
+rar a myfile.rar myfile
+rar a -p myfile.rar
+
+-------------------------
+
+ sudo apt install zip
+ mkdir myfile
+ zip -e myfile.zip myfile
+ ------------------------
+ 
+
+
